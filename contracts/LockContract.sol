78e29cdd4e2682a69572a42b5b4fe6f0d42ad9dc
JOAO MORAIS <goncalomorais96@homtail.com>
2022-08-10 14:05:19 +0100

    extra changes

--- contracts/LockContract.sol
+++ contracts/LockContract.sol
@@ -2,38 +2,49 @@
 // OpenZeppelin Contracts (last updated v4.7.0) (finance/LockContract.sol)
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/_token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
 /**
  * @title LockContract
- * @dev This contract handles the vesting of a ERC20 tokens for a given beneficiary. Custody of the token amount
- * can be given to this contract, which will release the token to the beneficiary following a given schedule.
+ * @dev This contract handles the vesting of a ERC20 tokens for a given beneficiary. Custody of the _token amount
+ * can be given to this contract, which will release the _token to the beneficiary following a given schedule.
  * The vesting schedule is established by a key->value pair in the form of _duration->_amounts. This two arrays
  * have the same length and are iterated by the _milestone variable
  */
 contract LockContract is Context {
 
-    event ERC20Released(address indexed token, uint256 amount);
+    //Events
+    event ERC20Released(address indexed _token, uint256 amount);
 
+    //Mappings
     mapping(address => uint256) private _erc20Released;
     mapping(address => Employee) public _walletToEmployee;
 
+    //Structs
     struct Employee {
-        address employee_address;
-        uint tokens_to_receive;
-        uint received_tokens;
-        bool employment_status;
+        address employee_address;//Probably can take it out
+
+        uint received_tokens;//Amount of tokens the employee has received
+
+        uint64 lock_start;//Saves the date of the initial locking of the contract
+
+        bool employment_status;//True-currently employed, False-no longer employed
+
+        bool og_employee;//Saves gas by id'ing each employee as og/true (the original locked team) 
+                         //or new/false (new employes). This way we can just save the durations and amounts
+                         //in two single variables in the contract instead of saving them for each og employee
     }
 
-    address token;
-    Employee[] employees;
-    uint32[]  _duration;
-    uint256[] _amounts;
+    //Variables
+    Employee[] _employees;
+    uint32[]  _duration;//Duration periods _duration[0]= 3 years, _duration[1]=3 years 1 month, etc
     uint256 _released;
-    uint _lockedTeamTokens;
+    uint256 _lockedTeamTokens;
+    uint256 _mileStone;
+    address _token;
 
 
 
@@ -44,18 +55,39 @@ contract LockContract is Context {
         address tokenAdress,
         address[] memory lockedTeamAddresses,
         address[] memory duration,
-        uint256 lockedTeamTokens
+        uint256[] lockedTeamTokens
     ) {
+        // create an employee struct for each OG employee
         for (i=0; i<lockedTeamAddresses.length; i++){
+
+            // employee address can't be the zero address
             require(lockedTeamAddresses[i] != address(0), "Constructor: locked team address is zero address");
-            employees.push(Employee(lockedTeamAddresses[i], lockedTeamTokens, 0, true));
+
+            // create the new employee struct
+            Employee memory employee = Employee(lockedTeamAddresses[i], 0, uint64(block.timestamp), true, true);
+
+            // push the new employee struct to the employees array
+            _employees.push(employee);
+
+            // map the new employee address to its struct
+            _walletToEmployee[msg.sender]=employee;
         }
 
-        token = tokenAdress;
+        // establish token address
+        _token = tokenAdress;
+
+        // establish the durations periods 
         _duration = duration;
-        _lockedTeamTokens = lockedTeamTokens;
     }
 
+
+    /**
+     * @dev Modifier that only allows current employess to interact with certain functions
+     */
+    modifier onlyEmployees(address _callerAddress){
+        require(_walletToEmployee[_callerAddress].employment_status == true, 'This address is no a current employee');
+        _;
+    }
 
 
     /**
@@ -67,10 +99,10 @@ contract LockContract is Context {
 
 
     /**
-     * @dev Amount of token already released
+     * @dev Amount of _token already released
      */
     function released() public view virtual returns (uint256) {
-        return _erc20Released[token];
+        return _erc20Released[_token];
     }
 
     /**
@@ -78,11 +110,11 @@ contract LockContract is Context {
      *
      * Emits a {ERC20Released} event.
      */
-    function release() public virtual {
+    function release() public virtual onlyEmployees(msg.sender){
         uint256 releasable = _vestingSchedule(uint64(block.timestamp));
-        _erc20Released[token] += releasable;
-        emit ERC20Released(token, releasable);
-        SafeERC20.safeTransfer(IERC20(token), beneficiary(), releasable);
+        _erc20Released[_token] += releasable;
+        emit ERC20Released(_token, releasable);
+        SafeERC20.safeTransfer(IERC20(_token), msg.sender, releasable);
     }
 
 
