28c90fbdbba79a6d46601ea142d8b5dc3ee17b2b
JOAO MORAIS <goncalomorais96@homtail.com>
2022-08-16 16:19:48 +0100

    updated with the correct wDHN token

--- contracts/LockContract.sol
+++ contracts/LockContract.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./IVotes.sol";
+import "./IERC20VotesAltered.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
@@ -18,7 +18,7 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 contract LockContract is Context {
 
     //Token
-    IVotes public _wDHN;
+    IERC20VotesAltered public _wDHN;
 
     //Events
     event ERC20Released(address indexed _token, uint256 amount);
@@ -62,7 +62,7 @@ contract LockContract is Context {
      * @dev Set the beneficiary, start timestamp and locking durations and amounts.
      */
     constructor(
-        IVotes wDHN,
+        IERC20VotesAltered wDHN,
         uint256 numMilestones,
         uint256 ogTeamTokens,
         uint256 initLock,
